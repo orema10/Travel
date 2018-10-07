be6de211218dc5b21e5e952d81721923a5665576
Orel star <galban81@gmail.com>
2018-10-07 09:35:44 +0300

    start project

---
+++ src/components/Filter.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="filter">
+        <div class="filter-container">
+            <input class="search" placeholder="Search ..." type="text" />
+            <countries></countries>
+            <citys></citys>
+            <button>Filter</button>
+        </div>
+    </section>
+</template>
+
+<script>
+import countries from './Countries';
+import citys from './Citys';
+export default {
+    components: {
+        countries,
+        citys
+    }
+}
+</script>
+
+<style scoped>
+section.filter {
+    position: fixed;
+    left: 0;
+    width: 15vw;
+    height: 100vh;
+    /* background-color: lightgray; */
+}
+
+section.filter .filter-container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    justify-content: space-around;
+}
+
+section.filter .filter-container input {
+    background: url('../assets/img/icons/MagnifyingGlass.svg');
+    background-repeat: no-repeat;
+    background-size: 15px 15px;
+    background-position: left;
+    padding-left: 20px;
+    padding-bottom: 3px;
+    border-bottom: 1px solid rgba(0,0,0,0.4);
+}
+
+section.filter .filter-container input::placeholder {
+    color: #333;
+    opacity: .7;
+    letter-spacing: .5px;
+}
+
+section.filter .filter-container input:-ms-input-placeholder {
+    color: #333;
+    opacity: .7;
+    letter-spacing: .5px;
+}
+
+section.filter .filter-container input::-ms-input-placeholder {
+    color: #333;
+    opacity: .7;
+    letter-spacing: .5px;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="header">
+        <header>
+            <div class="header-container">
+                <div id="nav">
+                <router-link to="/">Home</router-link>
+                <router-link to="/about">About</router-link>
+                </div>
+                <router-view/>
+            </div>
+        </header>
+        <div class="header-fixed"></div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style  scooped>
+header {
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 115px;
+  box-shadow: 0 2px 15px rgba(0, 0, 0, 0.12);
+  display: flex;
+  align-items: center;
+  color: #fff;
+  background: #333;
+  z-index: 3;
+}
+
+.header-fixed {
+    width: 100vw;
+    height: 115px;
+    z-index: 2;
+    background: #333;
+}
+
+.header-container {
+  width: 80vw;
+  margin: 0 auto;
+}
+
+header .header-container #nav a {
+  color: #fff;
+}
+
+header .header-container #nav a:hover::after  {
+  background-color: #fff;
+}
+
+#nav {
+  width: 20%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
